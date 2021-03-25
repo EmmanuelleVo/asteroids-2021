2d6e8d5ff8f4e9cbb23b1f693c60e214aa7320d2
thienanvo <thienan_vo@live.be>
2021-03-25 17:07:51 +0100

    fix controllers

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,7 +1,7 @@
 const controller = {
     keys : { // le nom de la touche et son effet
-        //'ArrowRight' : 1,
-        //'ArrowLeft' : -1,
+        'ArrowRight' : Math.PI / 180, // = 1 degré
+        'ArrowLeft' : -Math.PI / 180,
         'ArrowUp' : -1,
         //'ArrowDown' : 1
     },
@@ -9,20 +9,20 @@ const controller = {
 
     init() {
         document.addEventListener('keydown', (e) => {
-            e.preventDefault()
-            e.stopPropagation()
             // si la touche qu'on a pressée est inclue dans keys (on a extrait les clés de l'objet keys)
             // Càd qu'on limite la touche du clavier à <- et ->
             // + et que la touche n'est pas déjà dans le tableau
             if(Object.keys(this.keys).includes(e.key) && !this.activeKeys.includes(e.key)) {
+                e.preventDefault()
+                e.stopPropagation()
                 this.activeKeys.push(e.key)
             }
         })
         document.addEventListener('keyup', (e) => {
-            e.preventDefault()
-            e.stopPropagation()
             // si la touche est dans la tableau activeKeys, on la supprime du tableau
             if(this.activeKeys.includes(e.key)) {
+                e.preventDefault()
+                e.stopPropagation()
                 // indice de la touche qui a été pressée dans le tableau activeKeys
                 const i = this.activeKeys.indexOf(e.key)
                 this.activeKeys.splice(i, 1)
--- src/js/ship.js
+++ src/js/ship.js
@@ -1,12 +1,13 @@
 import controller from "./controller";
+import Vector from "./Vector";
 
 const ship = {
 
     size : 20,
-    speedX : 0,
-    speedY : 0,
-    locationX : 0,
-    locationY : 0,
+    speed : null,
+    heading: 0,
+    location : null,
+    acceleration : null,
     canvas : null,
     ctx : null,
 
@@ -14,33 +15,40 @@ const ship = {
         controller.init()
         this.canvas = canvas
         this.ctx = ctx
-        this.locationX = this.canvas.width / 2
-        this.locationY = this.canvas.height / 2
+        this.location = new Vector(this.canvas.width / 2,this.canvas.height / 2)
+        this.speed = new Vector(0,0)
+        this.acceleration = new Vector(0.05, 0.05)
     },
     update() {
         controller.activeKeys.forEach(activeKey => {
             // évalué à 1 ou -1 | valeur de la clé <- ou ->
             //this.speedX += controller.keys[activeKey] * 0.05
-            this.speedY += controller.keys[activeKey] * 0.05
+            if(activeKey === 'ArrowUp') {
+                this.speed.add(this.acceleration)
+            } else if(activeKey === 'ArrowRight' || activeKey === 'ArrowLeft') {
+                this.updateHeading(controller.keys[activeKey]) // le mettre à jour avec un certain angle
+            }
+
         })
 
         //this.locationX += this.speedX
-        this.locationY += this.speedY
-        /*if (this.locationX > this.canvas.width + this.size) { // + le centre du vaisseau
-            this.locationX = -this.size // on le repositionne à gauche du canvas
-        }*/
-        if (this.locationY > this.canvas.height + this.size) {
-            this.locationY = -this.size
+        this.location.add(this.speed)
+
+        if (this.location.y > this.canvas.height + this.size) { // + le centre du vaisseau
+            this.location.y = -this.size // on le repositionne à gauche du canvas
         }
-        if (this.locationY < -this.size) {
-            this.locationY = this.canvas.height + this.size
+        if (this.location.y < -this.size) {
+            this.location.y = this.canvas.height + this.size
         }
         this.draw()
 },
+    updateHeading(angle) {
+      this.heading += angle
+    },
     draw() { // Dessin du vaisseau
         this.ctx.save()
-        this.ctx.rotate(0)
-        this.ctx.translate(this.canvas.width / 2, this.locationY)
+        this.ctx.translate(this.canvas.width / 2, this.location.y)
+        this.ctx.rotate(this.heading)
         this.ctx.beginPath()
         this.ctx.moveTo(0, -1.5 * this.size / 2)
         this.ctx.lineTo(this.size / 2, 0.5 + (this.size * 1.5 / 2))
